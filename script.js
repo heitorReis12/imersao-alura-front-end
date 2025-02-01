c8b93cc78808c8bedebfc0205701733679bd3e99
Heitor <heitorredigolo9@gmail.com>
2025-01-31 21:19:33 -0300

    aula 4

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log('teste')
+const searchInput = document.getElementById('search-input');
+const resultArtist = document.getElementById("result-artist");
+const resultPlaylist = document.getElementById('result-playlists');
+
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => {
+            const filteredResults = result.filter(artist =>
+                artist.name.toLowerCase().includes(searchTerm)
+            );
+            displayResults(filteredResults);
+        })
+        .catch((error) => console.error("Erro ao buscar dados:", error));
+}
+
+function displayResults(result) {
+    resultPlaylist.classList.add("hidden")
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
+
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+
+    resultArtist.classList.remove('hidden');
+}
+
+document.addEventListener('input', function () {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === '') {
+        resultPlaylist.classList.remove('hidden');
+        resultArtist.classList.remove('hidden');
+        return
+    }
+    
+    requestApi(searchTerm);
+})
